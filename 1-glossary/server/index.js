7488646df3f5be58dcfdaae624cce4da80fa685a
Joy <parkerjn90@gmail.com>
2022-05-11 14:49:26 -0400

    establish skeleton functions for each type of request for each route

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -9,13 +9,28 @@ const app = express();
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/****
- *
- *
- * Other routes here....
- *
- *
- */
+app.get('/addwords', (req, res) => {
 
+});
+
+app.post('/addwords', (req, res) => {
+
+});
+
+app.get('/searchwords', (req, res) => {
+
+});
+
+app.post('/searchwords', (req, res) => {
+
+});
+
+app.patch('/searchwords', (req, res) => {
+
+});
+
+app.delete('/searchwords', (req, res) => {
+
+});
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
