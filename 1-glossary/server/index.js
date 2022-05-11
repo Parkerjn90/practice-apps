a9b41a40d76b38411b214c5708808a63e27eeec4
Joy <parkerjn90@gmail.com>
2022-05-11 15:41:35 -0400

    import and consume express.json middleware

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,36 +1,41 @@
 require("dotenv").config();
+console.log(process.env);
 const express = require("express");
 const path = require("path");
-const { postAdd, getAdd } = require("./handlers/addWord.js");
-const { postSearch, getSearch, patchSearch, deleteSearch } = require("./handlers/addWord.js");
+// const { postAdd, getAdd } = require("./handlers/addWord.js");
+// const { postSearch, getSearch, patchSearch, deleteSearch } = require("./handlers/addWord.js");
+const { color, log } = require('console-log-colors');
+const { bold, italic, underline, red, green, yellow, cyan, magenta, white } = color;
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-app.get('/addwords', (req, res) => {
-
-});
-
-app.post('/addwords', (req, res) => {
+app.get('/addword', (req, res) => {
 
 });
 
-app.get('/searchwords', (req, res) => {
+app.post('/addword', (req, res) => {
 
 });
 
-app.post('/searchwords', (req, res) => {
+app.get('/searchword', (req, res) => {
 
 });
 
-app.patch('/searchwords', (req, res) => {
+app.post('/searchword', (req, res) => {
 
 });
 
-app.delete('/searchwords', (req, res) => {
+app.patch('/searchword', (req, res) => {
 
 });
+
+app.delete('/searchword', (req, res) => {
+
+});
+
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
