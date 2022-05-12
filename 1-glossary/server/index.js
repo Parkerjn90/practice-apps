e8a050e115f1cb035e2d249013af4e219728ba85
Joy <parkerjn90@gmail.com>
2022-05-12 14:56:57 -0400

    added api call to free api dictionary https://dictionaryapi.dev/, changed /addword post route to call api, save data and first definition of response in db, and send back success message to client

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 console.log(process.env);
 const express = require("express");
 const path = require("path");
+const axios = require('axios');
 
 const { save, getOne, getAll, removeOne } = require('./db.js');
 // const { getAdd } = require("./handlers/addWord.js");
@@ -18,9 +19,14 @@ app.use(express.json());
 
 
 app.post('/addword', (req, res) => {
-  save(req.body.word, req.body.definition)
-    .then(() => {
-      res.status(201).send('word was saved');
+  axios({
+    method: 'get',
+    url: `https://api.dictionaryapi.dev/api/v2/entries/en/${req.body.word}`,
+    contentType: 'application/json'
+  }).then((wordEntry) => {
+    save(wordEntry.data[0].word, wordEntry.data[0].meanings[0].definitions[0].definition)
+  }).then((wordEntry1) => {
+      res.status(201).send('word was added');
     }).catch((err) => {
       res.status(404).send(`word already added \n ${err}`);
     });
