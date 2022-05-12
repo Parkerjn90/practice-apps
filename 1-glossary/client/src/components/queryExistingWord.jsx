dfd91402c2fc76cefddac869b6b9e2c77cd9dd4a
Joy <parkerjn90@gmail.com>
2022-05-12 17:25:25 -0400

    change to be functional component instead of class component

--- 1-glossary/client/src/components/queryExistingWord.jsx
+++ 1-glossary/client/src/components/queryExistingWord.jsx
@@ -2,47 +2,14 @@ import React from 'react';
 import {render} from 'react-dom';
 import axios from 'axios';
 
-class SearchWord extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      word: '',
-    }
-    // this.handleChange = this.handleChange.bind(this);
-    // this.clickHandler = this.clickHandler.bind(this);
-  }
+const SearchWord = (props) => {
 
-  // handleChange(e) {
-  //   e.preventDefault();
-  //   this.setState({word: e.target.value});
-  // }
-
-  // clickHandler(e) {
-  //   e.preventDefault();
-  //   axios({
-  //     method: 'post',
-  //     url: '/searchword',
-  //     data: this.state,
-  //     contentType: 'application/json'
-  //   })
-  //   .then((response) => {
-  //     console.log(response);
-  //     this.props.initialize(response.data);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   })
-  // }
-
-  render() {
     return (
       <div>
-        <p>Search Word:</p><input onChange={this.props.handleChange}/>
-        <button onClick={this.props.clickHandler}>Search</button>
-        <p>{this.state.response}</p>
+        <p>Search Word:</p><input onChange={props.handleChange}/>
+        <button onClick={props.clickHandler}>Search</button>
       </div>
-    )
-  }
+    );
 }
 
 export default SearchWord;
