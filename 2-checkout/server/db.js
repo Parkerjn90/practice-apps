9715967f7a74015ebdca0c035007669af1a0b417
Joy <parkerjn90@gmail.com>
2022-05-14 10:54:37 -0400

    update queryAsync function to create table if does not exist within deatabase, still have an error - need to specify var char maybe?

--- 2-checkout/server/db.js
+++ 2-checkout/server/db.js
@@ -16,25 +16,24 @@ db.connectAsync()
   .then(() =>
     // Expand this table definition as needed:
     db.queryAsync(
-      CREATE TABLE IF NOT EXISTS responses (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        session_id, // will need to make not null
-        username VARCHAR(100),
-        email,
-        password,
-        address_line1,
-        address_line2,
-        city,
-        state VARCHAR(2),
-        address_zip VARCHAR(5),
+      `CREATE TABLE IF NOT EXISTS responses (id INT NOT NULL AUTO_INCREMENT ,
+        session_id TEXT,
+        username TEXT,
+        email TEXT,
+        password TEXT,
+        address_line1 TEXT,
+        address_line2 TEXT,
+        city TEXT,
+        state TEXT,
+        address_zip INT,
         card_num NUMBER,
         exp_date DATE,
-        cvv VARCHAR(3),
-        billing_zip VARCHAR(5),
-      )
+        cvv INT,
+        billing_zip INT,
+        PRIMARY KEY ('id'))`
     )
   )
-  .then(() => console.log('success'));
-  .catch((err) => console.log(err));
+  .then(() => console.log('success'))
+  .catch((err) => console.log(err))
 
 module.exports = db;
